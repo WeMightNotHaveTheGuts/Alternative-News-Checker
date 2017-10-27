c4cef7ea8f8268cad80d464cad03c9e1ab0b09a4
Neil Boyd <conesco.3@gmail.com>
2017-10-27 21:04:33 +0100

    WebCheck

--- Alternate-News-Checker.py
+++ Alternate-News-Checker.py
@@ -1,8 +1,19 @@
+import requests
+import bs4
+
 def main():
     InputData = ArticleInput()
+    status = returnStatus()
+
 
 def ArticleInput():
     UserInput = input("Enter in fake news article: ")
     return UserInput
+
+def returnStatus():
+    return "Unknown"
+
+def WebCheck():
+    
 
 main()
