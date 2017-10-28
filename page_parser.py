2c44eb8741a04427aefb7f2d7db20e433412d201
Michael Mooney <mnmooney@users.noreply.github.com>
2017-10-28 15:59:28 +0100

    Add files via upload

--- page_parser.py
+++ page_parser.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from collections import deque
+from BeautifulSoup import BeautifulSoup
+import urllib2
 
 article_title = ""
 
@@ -14,6 +16,8 @@ def website(url): # Runs other functions
     value = longest_value(elems)
     article_title = ''.join(value)
     URL_check(url, value)
+    if isCredibleURL(URL) == False:
+        print "This is a fake news website"
 
 def longest_value(elems): # Finds the longest h1 tag in the webpage.
     longest = ""
@@ -46,6 +50,8 @@ def url_summarise(headline):
         i = i.lower()
     return headline
 
+
+
 def URL_check(url, longest):
     global article_title
     occurences = 0
@@ -61,7 +67,64 @@ def URL_check(url, longest):
     else:
         print "Article title might be " + article_title
 
+#Checking is the URL is legit
+        
+def isURLCredible(URL):
+    substrl = url_summarise(URL)
+    substrll = substrl[0]
+    if "www." in substrll:
+        substr = substrll[4:]
+    isLigitURL = True
+    fakeURLList = file('FakeNewsWebsite.txt')
+    for line in fakeURLList:
+        if substr in line:
+            isLigitURL = False
+    if isLigitURL == True:
+        print "Website is reliable"
+    else:
+        print "Wesite is not reliable"
+    return isLigitURL
 
+#Getting all links from the URL then seeing if there are any sources/if
+#any credible sources -- Note that this very inefficient
+#Not implemented into program yet
+
+def getAllLinks (url):
+    website = urllib2.urlopen(url)
+    html = website.read()
+    allLinks = re.findall('"((http|ftp)s?://.*?)"', html)
+    links = [seq[0] for seq in allLinks ]
+    sourcesReliable(url, links)
+
+#Refined for empty set rtee
+def url_summariseR(headline):
+    headline = re.split(r"[- |, /]", headline) #Removes -'s and /'s from URL
+    if len(headline) == 1:
+        return headline
+    for i in range(2):
+        headline.pop(0) # removes blank list entry and http
+        if len(headline) == 1:
+            return headline
+    if ".html" or ".htm" or ".php" in headline[0,-1]: # removes file endings if there are any
+        headline.pop(-1)
+    for i in headline:
+        i = i.lower()
+    return headline
+
+def sourcesReliable (url, links):
+    reliableSources = False
+    for i in range(len(links)):
+        thing = url_summariseR(links[i-1])
+        newThing = thing[0]
+        if "www." in newThing:
+            newThing = newThing[4:]
+        if newThing not in url:
+            for line in file('ReliableNewsWebsites.txt'):
+                if newThing in line:
+                    reliableSources = True
+    return reliableSources
+
+                
 #website("http://www.independent.co.uk/news/business/google-amazon-profits-latest-earning-reports-billions-a8022286.html")
 #website("http://www.independent.co.uk/news/uk/politics/jeremy-corbyn-sexual-assault-is-parliament-thrives-mps-politicians-a8024026.html")
 #website("https://www.infowars.com/joy-villa-for-congress-trump-approves/")
