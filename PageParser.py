75ed56cf5e028d559d52cf4ae478cacde42b9f7a
conesco3 <conesco.3@gmail.com>
2017-10-28 07:42:59 +0100

    Finished title searching program

--- PageParser.py
+++ PageParser.py
@@ -3,15 +3,19 @@ from bs4 import BeautifulSoup
 import re
 from collections import deque
 
-def Website(url):
+ArticleTitle = ""
+
+def Website(url): # Runs other functions
     res = requests.get(url)
     res.raise_for_status()
     content = BeautifulSoup(res.text, "html.parser")
     elems = content.select('h1')
     value = longestValue(elems)
+    global ArticleTitle
+    ArticleTitle = ''.join(value)
     URLCheck(url, value)
 
-def longestValue(elems):
+def longestValue(elems): # Finds the longest h1 tag in the webpage.
     longest = ""
     for i in elems:
         i = i.getText()
@@ -29,29 +33,38 @@ def summarise(headline):
         for word in head_words:
             word = re.sub('\W', '', word)  # strip non-alphanumeric characters from word
             if word not in stop:  # don't include any stopwords
-                headline_summary.append(word)
+                headline_summary.append(str(word).lower())
     return headline_summary
 
 def urlsummarise(headline):
-    headline = re.split(r"[- |, /]", headline)
+    headline = re.split(r"[- |, /]", headline) #Removes -'s and /'s from URL
     for i in range(2):
-        headline.pop(0)
-    if ".html" or ".htm" or ".php" in headline[0,-1]:
+        headline.pop(0) # removes blank list entry and http
+    if ".html" or ".htm" or ".php" in headline[0,-1]: # removes file endings if there are any
         headline.pop(-1)
+    for i in headline:
+        i = i.lower()
     return headline
 
 def URLCheck(url, longest):
+    occurences = 0
     value = summarise(longest)
     urlSum = urlsummarise(url)
     domain = urlSum[0]
     urlSum.pop(0)
-    if value == urlSum:
-        print "True"
+    for i in value:
+        if i in urlSum:
+            occurences += 1
+    if occurences > len(value)/4:
+        global ArticleTitle
+        print "Article title is " + ArticleTitle
     else:
-        print "False"
+        global ArticleTitle
+        print "Article title might be " + ArticleTitle
+
 
 
 Website("http://www.independent.co.uk/news/business/google-amazon-profits-latest-earning-reports-billions-a8022286.html")
 Website("http://www.independent.co.uk/news/uk/politics/jeremy-corbyn-sexual-assault-is-parliament-thrives-mps-politicians-a8024026.html")
 Website("https://www.infowars.com/joy-villa-for-congress-trump-approves/")
-Website("http://www.milngavieherald.co.uk/news/could-you-give-bruiser-his-forever-home-1-4598319")
+Website("http://www.bbc.co.uk/news/world-europe-41785292")
