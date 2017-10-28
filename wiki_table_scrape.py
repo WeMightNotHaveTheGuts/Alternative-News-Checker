f3f7a7695eec9a6aaa1cb296f227276fb9df111f
LewisDyer <33163858+LewisDyer@users.noreply.github.com>
2017-10-28 05:49:28 +0100

    scraped satirical sites
    Modified table scrape to get list of satirical sites as well (feeling like a hackjob code monkey rn). Implement re substitution to get rid of references in notes.

--- wiki_table_scrape.py
+++ wiki_table_scrape.py
@@ -6,17 +6,29 @@ import re
 url = "https://en.wikipedia.org/wiki/List_of_fake_news_websites"
 
 res = requests.get(url)
-res.raise_for_status() #raises an exception for error codes#
+res.raise_for_status()  # raises an exception for error codes
 wikiPage = BeautifulSoup(res.text, "html.parser")
 websites = wikiPage.select('.wikitable')
 rows = websites[0].select('td')
-rows_text= []
-fake_wiki_data= []
+rows_text = []
+fake_wiki_data = []
 for i in range(len(rows)):
-    if i % 3 != 2:
-        rows_text.append(rows[i].getText()) # implement re.sub before this to get rid of references
+    if i % 3 != 2:  # only get rows that aren't in the sources column
+        rows_text.append(re.sub("\[\d*\]", "", rows[i].getText()))  # implement re.sub before this to get rid of references
 for j in range(0, len(rows_text), 2):
-     fake_wiki_data.append([rows_text[j], rows_text[j+1]])
+    fake_wiki_data.append([rows_text[j], rows_text[j+1]])
 
-print fake_wiki_data[1]
+url = "https://en.wikipedia.org/wiki/List_of_satirical_news_websites"
 
+res = requests.get(url)
+res.raise_for_status()
+wikiPage = BeautifulSoup(res.text, "html.parser")
+websites = wikiPage.select('.wikitable')
+rows = websites[0].select('td')
+joke_wiki_data = []
+for i in range(len(rows)):
+    if i % 3 == 0:
+        joke_wiki_data.append(rows[i].getText())
+
+# fake_wiki_data - list of fake sites with some notes for each site, in a list of 2-element lists.
+# joke_wiki_data - list of known satirical sites
